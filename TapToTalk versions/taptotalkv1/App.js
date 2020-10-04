31562a7a2a1e86b2f843239a15ea3d61a1ff4738
Aman Agarwal <amanagarwalv1.0@gmail.com>
2020-10-04 16:59:35 +0530

    firstcommit

---
+++ TapToTalk versions/taptotalkv1/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import {View,Text} from 'react-native';
+//import {GoogleSignin,GoogleSigninButton,statusCodes} from 'react-native-google-signin';
+
+export default class App extends Component 
+{
+  render()
+  {
+    return(
+      <View>
+        <Text>
+            hey
+        </Text>
+      </View>
+    );
+  }
+}
