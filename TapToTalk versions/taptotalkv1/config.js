1f84e92c53afe0b0e0ad5773e3b98b4a5201d1c1
Priyanshu Mittal <mittalpriyanshu19@gmail.com>
2020-10-04 20:09:02 +0530

    google signin added

---
+++ TapToTalk versions/taptotalkv1/config.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyAGHQX0J8BcFLZrvxIWWTqpZTZrx3v-8hU",
+    authDomain: "taptotalk-1b199.firebaseapp.com",
+    databaseURL: "https://taptotalk-1b199.firebaseio.com",
+    projectId: "taptotalk-1b199",
+    storageBucket: "taptotalk-1b199.appspot.com",
+    messagingSenderId: "378754635240",
+    appId: "1:378754635240:web:ebfec6b33638c05043005d",
+    measurementId: "G-HBSH8ZQ4K4"
+  };
